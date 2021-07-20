8dfee40b8090c562b8de2348a8db050d453810c2
mflem8 <matthewfleming8@gmail.com>
2021-07-19 20:53:55 -0400

    update parts controller

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,8 +1,22 @@
 class PartsController < ApplicationController
 
     def index
-        notes = Note.all
-        render json: @notes 
+        parts = Part.all
+        render json: @parts
     end
-    
+
+    def show
+        part = Part.find(params[:id])
+        render json: @part
+    end
+
+    def create
+        part = Part.create(part_params)
+    end
+
+    private
+    def part_params
+        params.require(:part).permit(:name)
+    end
+
 end
